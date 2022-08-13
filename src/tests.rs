3b29a8574257e6f7e2d9c483e0a9f1be62375adb
0xhades <OnlyGhostz@gmail.com>
2022-08-13 06:12:28 +0300

    captliaize `TIMEOUT`

--- src/tests.rs
+++ src/tests.rs
@@ -8,12 +8,12 @@ mod test {
     use reqwest::Proxy;
 
     const SESSION_ID: &str = "MISSING";
-    const timeout: Duration = Duration::from_secs(10);
+    const TIMEOUT: Duration = Duration::from_secs(10);
 
     #[ignore = "sessions aren't available"]
     #[tokio::test]
     async fn get_profile() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let get_profile = APIs::new(APIs::CurrentUser(String::from(SESSION_ID)));
 
         let resp = client.execute(&get_profile, None).await.unwrap();
@@ -25,7 +25,7 @@ mod test {
     #[ignore = "sessions aren't available"]
     #[tokio::test]
     async fn edit_profile() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let get_profile = APIs::new(APIs::CurrentUser(String::from(SESSION_ID)));
 
         let resp = client.execute(&get_profile, None).await.unwrap();
@@ -44,7 +44,7 @@ mod test {
 
     #[tokio::test]
     async fn check_username() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let check_username = APIs::new(APIs::CheckUsername);
 
         let username = UsernameBuilder::new().single("0xhadesssapfpes").build();
@@ -58,7 +58,7 @@ mod test {
 
     #[tokio::test]
     async fn create() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let create = APIs::new(APIs::Create);
 
         let username = UsernameBuilder::new().single("0xhadesssapfpes").build();
@@ -69,7 +69,7 @@ mod test {
 
     #[tokio::test]
     async fn create_business_validated() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let CreateBusinessValidated = APIs::new(APIs::CreateBusinessValidated);
 
         let username = UsernameBuilder::new().single("0xhadesssapfpes").build();
@@ -83,7 +83,7 @@ mod test {
 
     #[tokio::test]
     async fn create_secondary_account() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let CreateSecondaryAccount = APIs::new(APIs::CreateSecondaryAccount);
 
         let username = UsernameBuilder::new().single("0xhadesssapfpes").build();
@@ -97,7 +97,7 @@ mod test {
 
     #[tokio::test]
     async fn web_create_ajax() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let WebCreateAjax = APIs::new(APIs::WebCreateAjax);
 
         let username = UsernameBuilder::new()
@@ -113,7 +113,7 @@ mod test {
 
     #[tokio::test]
     async fn Username_suggestions() {
-        let client = Client::new(timeout, None).unwrap();
+        let client = Client::new(TIMEOUT, None).unwrap();
         let UsernameSuggestions = APIs::new(APIs::UsernameSuggestions);
 
         let username = UsernameBuilder::new()
